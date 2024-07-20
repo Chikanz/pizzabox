e9b085eeb64ebc26ad9fd3e0fe567dc3fd145f6e
Zac Lucarelli <Chikanz@users.noreply.github.com>
2024-07-20 12:29:09 +1000

    Initial commit

---
+++ Scripts/PolyPizza/APIManager.cs
@@ -0,0 +1,224 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using Cysharp.Threading.Tasks;
+using GLTFast;
+using UnityEngine;
+using UnityEngine.Networking;
+using PolyPizza;
+using UnityEngine.Serialization;
+
+namespace PolyPizza
+{
+    public class APIManager : MonoBehaviour
+    {
+        public static APIManager instance;
+
+        private const float MIN_BOUNDING_BOX_SIZE_FOR_SIZE_FIT = 0.001f;
+
+        const string URL = "https://api.poly.pizza/v1/";
+        public string APIKEY;
+
+        [FormerlySerializedAs("CacheToFile")] public bool CacheModelsToFile = true;
+
+        private async void Awake()
+        {
+            if (!instance) instance = this;
+
+            if (APIKEY == string.Empty)
+            {
+                Debug.LogError("API key wasn't set. Please get a key from https://poly.pizza/settings/api");
+                enabled = false;
+            }
+        }
+
+        //stolen from old poly API code lol
+        //returns false if the model is too small to scale
+        static bool ComputeScaleFactorToFit(Bounds bounds, float desiredSize, out float scaleFactor)
+        {
+            float biggestSide = Math.Max(bounds.size.x, Math.Max(bounds.size.y, bounds.size.z));
+            if (biggestSide < MIN_BOUNDING_BOX_SIZE_FOR_SIZE_FIT)
+            {
+                scaleFactor = 1.0f;
+                return false;
+            }
+
+            scaleFactor = desiredSize / biggestSide;
+            return true;
+        }
+
+
+        /// <summary>
+        /// Take in model JSON data and spawn a game object
+        /// </summary>
+        public async UniTask<GameObject> MakeModel(Model model, float scale = 1, bool positionCenter = false)
+        {
+            var newObj = new GameObject("model_" + model.Id);
+            var gltf = newObj.AddComponent<GltfBoundsAsset>();
+            gltf.LoadOnStartup = false;
+
+            var url = model.Download.ToString();
+            if (CacheModelsToFile) //Load model from file if it exists
+            {
+                string savePath = string.Format("{0}/{1}.glb", Application.persistentDataPath, model.Id);
+
+                if (!File.Exists(savePath))
+                {
+                    using (var client = new WebClient())
+                    {
+                        await client.DownloadFileTaskAsync(new Uri(url), savePath);
+                    }
+                }
+
+                url = savePath;
+            }
+
+            //Load model from HDD or URL
+            try
+            {
+                await gltf.Load(url);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Error loading " + url);
+                return null;
+            }
+
+            var box = newObj.GetComponent<BoxCollider>();
+
+            if (ComputeScaleFactorToFit(gltf.Bounds, scale, out float scaleFactor))
+            {
+                newObj.transform.localScale = Vector3.one * scaleFactor;
+            }
+
+            if (positionCenter)
+            {
+                Vector3 offset = newObj.transform.position - box.center;
+                newObj.transform.position -= offset;
+            }
+
+            return newObj;
+        }
+
+        private UnityWebRequestAsyncOperation GetReq(string url)
+        {
+            UnityWebRequest www = UnityWebRequest.Get(url);
+            www.SetRequestHeader("X-Auth-Token", APIKEY);
+            return www.SendWebRequest();
+        }
+
+        /// <summary>
+        /// Given a model ID, get the model from the Poly Pizza API
+        /// 
+        public async UniTask<Model> GetModelByID(string id)
+        {
+            var res = await GetReq($"{URL}/model/{id}");
+
+            if (res.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(res.downloadHandler.text);
+                return null;
+            }
+            else
+            {
+                return Model.FromJson(res.downloadHandler.text);
+            }
+        }
+
+        /// <summary>
+        /// Search the Poly Pizza API for a model matching the search query
+        /// </summary>
+        /// <param name="keyword">The model you want to search for, eg Apple, Monkey, Gun etc </param>
+        /// <returns></returns>
+        public async UniTask<Model> GetExactModel(string keyword)
+        {
+            if (keyword.Length <= 2) return null;
+
+            var res = await GetReq($"{URL}/search/{keyword}");
+            if (res.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError(res.downloadHandler.text);
+                return null;
+            }
+            else
+            {
+                var search = SearchResults.FromJson(res.downloadHandler.text);
+                if (search.Total == 0) return null;
+
+                var regex = new Regex($"(s|{keyword})", RegexOptions.IgnoreCase);
+                foreach (var model in search.Results)
+                {
+                    // if (String.Equals(model.Title, keyword, StringComparison.CurrentCultureIgnoreCase)) return model;
+                    if (regex.Match(model.Title).Success)
+                        return model;
+                }
+
+                return search.Results[0];
+            }
+        }
+
+        /// <summary>
+        /// Get the top models of all time from the Poly Pizza API
+        /// </summary>
+        /// <param name="limit">How many models to get. I think this caps at like 500 or something</param>
+        /// <returns>Model object array</returns>
+        public async UniTask<Model[]> GetPopular(int limit)
+        {
+            int pagesToGet = Mathf.CeilToInt(limit / 32.0f);
+            List<Model> models = new List<Model>();
+            for (int i = 0; i < pagesToGet; i++)
+            {
+                var res = await getURL($"{URL}/search?limit=32&page={i}");
+                if (res != null)
+                {
+                    var search = SearchResults.FromJson(res);
+                    models.AddRange(search.Results);
+                }
+            }
+
+            return models.ToArray();
+        }
+
+        private async UniTask<string> getURL(string url)
+        {
+            //file cache
+            string filename = WebUtility.UrlEncode(url);
+            string reqPath = string.Format("{0}/{1}", Application.persistentDataPath, filename);
+            
+            if (CacheModelsToFile)
+            {
+                if (File.Exists(reqPath))
+                {
+                    return File.ReadAllText(reqPath);
+                }
+            }
+
+            //Hit server
+            var res = await GetReq(url);
+            if (res.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError(res.downloadHandler.text);
+                return null;
+            }
+            else
+            {
+                File.WriteAllText(reqPath, res.downloadHandler.text);
+                return res.downloadHandler.text;
+            }
+        }
+
+        /// <summary>
+        /// API returns the orbit of the camera, so we convert it to a rotation using
+        /// https://stackoverflow.com/questions/48841969/theta-phi-to-quaternion-rotation-unity3d-c-sharp
+        /// and then ignoring the x component and inverting the y component since the orbit is for the camera not model
+        /// </summary>
+        public static Quaternion OrbitToRotation(Orbit orbit)
+        {
+            return Quaternion.Euler(0, (Mathf.Rad2Deg * orbit.theta), 0f);
+        }
+    }
+}
---
+++ Scripts/PolyPizza/Spawner.cs
@@ -0,0 +1,204 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cysharp.Threading.Tasks;
+using PolyPizza;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace PolyPizza
+{
+    [RequireComponent(typeof(APIManager))]
+    public class Spawner : MonoBehaviour
+    {
+        public int width;
+        public int height;
+        public float spacing;
+
+        [FormerlySerializedAs("platform")] [Tooltip("Pedestal object for models to spawn on.")]
+        public GameObject pedestal;
+
+        private List<Transform> platforms = new List<Transform>();
+        private Transform[,] plane;
+
+        private float platformObjWidth;
+        // [SerializeField] private float spawnerDelaySpacing = 1;
+        // private float platformObjHeight;
+
+        [Tooltip("Model IDs to exclude from the spawner.")]
+        public string[] Exclude;
+
+        private static readonly int Metallic = Shader.PropertyToID("metallicFactor");
+        private static readonly int Roughness = Shader.PropertyToID("roughnessFactor");
+
+        void Awake()
+        {
+            //Create a bunch of platforms for the models to spawn on 
+            plane = new Transform[width, height];
+            platformObjWidth = pedestal.GetComponent<BoxCollider>().size.x;
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    var platform = Instantiate(pedestal,
+                        transform.position + new Vector3((((platformObjWidth + spacing) * i)), 0,
+                            (((platformObjWidth + spacing) * j))),
+                        Quaternion.identity);
+                    platforms.Add(platform.transform);
+                    plane[i, j] = platform.transform;
+                    platform.transform.SetParent(transform); //Child to the spawner
+                }
+            }
+
+            //See if we can hit floor
+            foreach (Transform t in platforms.ToList())
+            {
+                if (Physics.SphereCast(t.position, platformObjWidth / 4, Vector3.down, out RaycastHit hit))
+                {
+                    if (!hit.collider.tag.Equals("Ground"))
+                    {
+                        Destroy(t.gameObject);
+                        platforms.Remove(t);
+                    }
+                    else
+                    {
+                        t.position = hit.point;
+                    }
+                }
+                else
+                {
+                    Destroy(t.gameObject);
+                    platforms.Remove(t);
+                }
+            }
+        }
+
+        private void Start()
+        {
+            Spawn();
+        }
+
+        private async void Spawn()
+        {
+            var models = await APIManager.instance.GetPopular(platforms.Count + Exclude.Length);
+
+            //exclude models
+            models = Array.FindAll(models, (Model m) => !Exclude.Contains(m.Id)).ToArray();
+
+            //Remove high poly models
+            models = Array.FindAll(models, (Model m) => m.TriCount <= 8000).ToArray();
+
+            var modelTaskDone = new bool[models.Length];
+            var order = spiralOrder(plane); //Spawn them in a spiral because fuck it we fancy
+            order.Reverse(); //start with most popular in center
+            int _nextPlat = 0;
+            for (int i = 0; i < platforms.Count; i++)
+            {
+                if (i >= models.Length) continue;
+                GameObject model =
+                    await APIManager.instance.MakeModel(models[i], platformObjWidth * 0.9f, false); //Spawn the model
+                if (model == null) continue; //Model is fucked
+                ToggleRenderers(model, false);
+
+                //Find a non null platform to use lol
+                //platform.transform.GetChild(0) is the platform pivot where the model is spawned
+                //so we're checking if we've spawned something into a platform before
+                var platform = order[_nextPlat];
+                _nextPlat++;
+                while (platform == null || platform.transform.GetChild(0).childCount > 0)
+                {
+                    platform = order[_nextPlat];
+                    _nextPlat++;
+                }
+
+                //without SyncTransforms() the box collider hasn't been updated in physics will fuck up the placement
+                Physics.SyncTransforms();
+                var modelCollider = model.GetComponent<BoxCollider>();
+
+                var newPiv = new GameObject(models[i].Id + " Pivot");
+                newPiv.transform.SetParent(platform.transform.GetChild(0));
+
+                //Make a pivot for the model on the bottom center face of the bounds
+                newPiv.transform.position =
+                    modelCollider.bounds.center + (Vector3.down * modelCollider.bounds.size.y / 2);
+                model.transform.SetParent(newPiv.transform, true);
+
+                newPiv.transform.localPosition = Vector3.zero;
+                var rot = APIManager.OrbitToRotation(models[i].Orbit);
+                newPiv.transform.rotation = rot;
+                ToggleRenderers(model, true);
+                MakeMatte(model);
+
+                //Play animation
+                if (model.TryGetComponent(out Animation anim))
+                {
+                    anim.Play();
+                }
+
+                modelTaskDone[i] = true;
+            }
+        }
+
+        private void ToggleRenderers(GameObject obj, bool on)
+        {
+            var renderers = obj.GetComponentsInChildren<Renderer>();
+            foreach (var r in renderers)
+            {
+                r.enabled = on;
+            }
+        }
+
+        private void MakeMatte(GameObject obj)
+        {
+            var renderers = obj.GetComponentsInChildren<Renderer>();
+            foreach (var r in renderers)
+            {
+                foreach (Material mat in r.sharedMaterials)
+                {
+                    mat.SetFloat(Metallic, 0);
+                    mat.SetFloat(Roughness, 1);
+                }
+            }
+        }
+
+        //https://www.geeksforgeeks.org/print-a-given-matrix-in-spiral-form/
+        public static List<T> spiralOrder<T>(T[,] matrix)
+        {
+            List<T> ans = new List<T>();
+
+            if (matrix.Length == 0)
+                return ans;
+
+            int R = matrix.GetLength(0), C = matrix.GetLength(1);
+            bool[,] seen = new bool[R, C];
+            int[] dr = { 0, 1, 0, -1 };
+            int[] dc = { 1, 0, -1, 0 };
+            int r = 0, c = 0, di = 0;
+
+            // Iterate from 0 to R * C - 1
+            for (int i = 0; i < R * C; i++)
+            {
+                ans.Add(matrix[r, c]);
+                seen[r, c] = true;
+                int cr = r + dr[di];
+                int cc = c + dc[di];
+
+                if (0 <= cr && cr < R && 0 <= cc && cc < C
+                    && !seen[cr, cc])
+                {
+                    r = cr;
+                    c = cc;
+                }
+                else
+                {
+                    di = (di + 1) % 4;
+                    r += dr[di];
+                    c += dc[di];
+                }
+            }
+
+            return ans;
+        }
+    }
+}
